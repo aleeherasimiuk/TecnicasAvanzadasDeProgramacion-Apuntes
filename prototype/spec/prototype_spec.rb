c89b64812b8803faf707358d17bb6bd0d464099f
Alee Herasimiuk <aleeherasimiuk@gmail.com>
2021-09-23 10:43:20 -0300

    Using method missing when using property not defined

--- prototype/spec/prototype_spec.rb
+++ prototype/spec/prototype_spec.rb
@@ -11,13 +11,21 @@ class PrototypeObject
   def get_property(property_name)
     @properties.fetch(property_name) {raise PropertyNotFoundError.new}
   end
+
+  def method_missing(method_name, *params, &block)
+    if @properties.has_key?(method_name)
+      get_property(method_name)
+    else
+      super
+    end
+  end
 end
 
 class PropertyNotFoundError < StandardError
 end
 
-describe '' do
-  it '' do
+describe 'Prototyped Objects' do
+  it 'should set/get property' do
 
     guerrero = PrototypeObject.new
     guerrero.set_property(:energia, 100)
@@ -26,11 +34,29 @@ describe '' do
 
   end
 
-  it '' do
+  it 'should raise error when property is undefined' do
 
     guerrero = PrototypeObject.new
 
     expect{guerrero.get_property(:energia)}.to raise_error(PropertyNotFoundError)
 
   end
+
+  it 'should define methods for properties' do
+
+    guerrero = PrototypeObject.new
+    guerrero.set_property(:energia, 100)
+
+    expect(guerrero.energia).to eq 100
+
+  end
+
+  it 'should raise NoMethodError if property is not defined' do
+
+    guerrero = PrototypeObject.new
+
+    expect{ guerrero.energia }.to raise_error(NoMethodError)
+
+  end
+
 end
